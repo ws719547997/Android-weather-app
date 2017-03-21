5bd910136dbd3064ce88857bfb497a5ab4c82a2d
ws <ws719547997@gmail.com>
2017-03-21 17:08:30 +0800

    test git 2

---
+++ app/src/main/java/com/example/ws719547997/weathertest/db/City.java
@@ -0,0 +1,47 @@
+package com.example.ws719547997.weathertest.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ws719547997 on 2017/3/21.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/ws719547997/weathertest/db/County.java
@@ -0,0 +1,47 @@
+package com.example.ws719547997.weathertest.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ws719547997 on 2017/3/21.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int ciytId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCiytId() {
+        return ciytId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCiytId(int ciytId) {
+        this.ciytId = ciytId;
+    }
+}
